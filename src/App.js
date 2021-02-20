0af53a7d7f72b82632c7b03090387d4b42296584
kiran nk <kiran.nk@itcinfotech.com>
2021-02-20 13:27:54 +0530

    Added github pages configuration

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
 		);
 		return (
 			<div className="App">
+				<h1>Mosnters Rolodex</h1>
 				<SearchBox placeholder="Search Monsters" handleChange={this.handleChange} />
 				<CardList monsters={filteredMonsters} />
 			</div>
